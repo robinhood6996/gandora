176b66ead0cc92abd759f8f44a25504f65e33cae
robinhood6996 <robinypb@yahoo.com>
2022-06-04 23:16:19 +0600

    fav

--- src/index.tsx
+++ src/index.tsx
@@ -27,6 +27,7 @@ import ComingSoon from "./pages/Upcoming/ComingSoon";
 import AuthProvider from "./context/AuthProvider";
 import Activity from "./components/Activity/Activity";
 import Offers from "./components/Offers/Offers";
+import Favourite from "./components/Favourite/Favourite";
 // const container = document.getElementById('root');
 // // Create a root.
 // const root = ReactDOM.createRoot(container);
@@ -63,6 +64,7 @@ ReactDOM.render(
             <Route path="swap" element={<ComingSoon />} />
             <Route path="activity" element={<Activity />} />
             <Route path="offers" element={<Offers />} />
+            <Route path="favourite" element={<Favourite />} />
           </Routes>
         </Layout>
       </Provider>
