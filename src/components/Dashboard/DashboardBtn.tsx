7a3299c48a5d2159ed4d0ac11e9f970c5fc08b42
Ruhul Amin Hira <ruhulaminhira63@gmail.com>
2022-06-05 21:51:10 +0600

    working on dashboard

--- src/components/Dashboard/DashboardBtn.tsx
+++ src/components/Dashboard/DashboardBtn.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const DashboardBtn = () => {
+const DashboardBtn = ({ title, icon, adiClass = "", active = false }: any) => {
+    const styleActive = active ? "text-white bg-default" : "border-borderdefault text-davygrey"
     return (
-        <button className='border-[0.5px] border-[#C4C4C4] py-1 px-2 rounded-sm w-[170px]'> <span>*</span> item</button>
+        <button className={`border-[0.5px]  p-2 rounded-[5px] w-[170px] ${adiClass} ${styleActive} flex  justify-center items-center uppercase text-[15px] font-Rajdhani font-semibold hover:text-white hover:bg-default `}> <span className='pr-6'>{icon}</span>{title}</button>
     )
 }
 
--- src/components/Dashboard/DashboardOptions.tsx
+++ src/components/Dashboard/DashboardOptions.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
-import { AiFillCaretRight } from "react-icons/ai";
+import { AiFillCaretRight, AiFillStar } from "react-icons/ai";
+import { RiCheckboxMultipleBlankFill } from "react-icons/ri";
+import { BsFillSuitHeartFill, BsFillTagFill } from "react-icons/bs";
+import { MdOutlineSettingsBackupRestore } from "react-icons/md";
+import { } from "react-icons/bs";
 import DashboardBtn from './DashboardBtn';
+import SearchInput from './SearchInput';
+import DashboardDropDown from './DashboardDropDown';
 
 const DashboardOptions = () => {
+    const marginStyle = "mr-10"
     return (
         <div className=''>
-            <h1 className='flex text-[34px] text-default font-Rajdhani font-semibold items-center'> <span className='text-[20px]'><AiFillCaretRight /></span> Dashboard</h1>
+            <h1 className='flex text-[34px] text-default font-Rajdhani font-semibold items-center tracking-widest'> <span className='text-[20px]'><AiFillCaretRight /></span> Dashboard</h1>
+            <div className='flex w-[100%] md:w-[70%] justify-between items-center mx-auto'>
+                <DashboardBtn title={"Item"} icon={<AiFillStar />} adiClass={marginStyle} />
+                <DashboardBtn title={"Collection"} icon={<RiCheckboxMultipleBlankFill />} adiClass={marginStyle} />
+                <DashboardBtn title={"Favorite"} icon={<BsFillSuitHeartFill />} adiClass={marginStyle} />
+                <DashboardBtn title={"Activity"} icon={<MdOutlineSettingsBackupRestore />} adiClass={marginStyle} />
+                <DashboardBtn title={"Offers"} icon={<BsFillTagFill />} />
+            </div>
             <div className='flex'>
-                <DashboardBtn />
-                <DashboardBtn />
-                <DashboardBtn />
-                <DashboardBtn />
-                <DashboardBtn />
+                <div>
+                    <SearchInput />
+                </div>
+                <div>
+                    <DashboardDropDown />
+                </div>
+                <div>
+                    <DashboardDropDown />
+                </div>
+
             </div>
         </div>
     )
